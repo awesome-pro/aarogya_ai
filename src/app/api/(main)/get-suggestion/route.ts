2dfbd1cd88f51c203f61e72fd07c7c8c30feddd6
abhinandan-verma <abhinandanverma551@gmail.com>
2024-05-16 22:21:13 +0530

    "abhi" branch created

---
+++ src/app/api/(main)/get-suggestion/route.ts
@@ -0,0 +1,53 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = 'edge';
+
+export async function POST(req: Request) {
+  try {
+    const prompt =
+      "Suggest me best doctor depart "
+    const response = await openai.completions.create({
+      model: 'gpt-3.5-turbo-instruct',
+      max_tokens: 400,
+      stream: true,
+      prompt,
+    });
+
+    const stream = OpenAIStream(response);
+
+    
+    if(!stream){
+        console.log("response not found: ", response)
+        return NextResponse.json(
+            {
+                "message": "response not found"
+            },
+            {
+                status: 500,
+                statusText: "Server Error"
+            }
+        )
+    }
+    
+    
+    return new StreamingTextResponse(stream);
+
+    
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      // OpenAI API error handling
+      const { name, status, headers, message } = error;
+      return NextResponse.json({ name, status, headers, message }, { status });
+    } else {
+      // General error handling
+      console.error('An unexpected error occurred:', error);
+      throw error;
+    }
+  }
+}

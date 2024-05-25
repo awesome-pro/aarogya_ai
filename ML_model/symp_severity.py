335039852f341c99bce8c3ceebbf983260920ba2
Vishal Chaurasiya <129888819+Vishal35198@users.noreply.github.com>
2024-05-25 12:37:46 +0530

    some_issue_fixed

--- ML_model/symp_severity.py
+++ ML_model/symp_severity.py
@@ -5,7 +5,6 @@ df = pd.read_csv('Symptom-severity.csv')
 df['Symptom'] = df['Symptom'].str.replace('_',' ')
 # df['Symptom'] = df['Symptom'].str.strip()
 
-
 def get_rank(symptoms):
     filt = df[df['Symptom'] == symptoms]
     return filt['weight'].values[0]

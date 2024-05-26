63df8d92f16e53afda2305ef873c5ca14fc56e14
Vishal Chaurasiya <129888819+Vishal35198@users.noreply.github.com>
2024-05-26 10:31:56 +0530

    done

--- ML_model/symp_severity.py
+++ ML_model/symp_severity.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('Symptom-severity.csv')
 
 df['Symptom'] = df['Symptom'].str.replace('_',' ')
-# df['Symptom'] = df['Symptom'].str.strip()
+df['Symptom'] = df['Symptom'].str.strip()
 
 
 def get_rank(symptoms):
